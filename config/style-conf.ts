3c24074b4b869706d51efd115b9e2b24f3df7396
blitty <blittynet@gmail.com>
2021-04-18 18:44:52 +0200

    New palette of colors

--- config/style-conf.ts
+++ config/style-conf.ts
@@ -1,1 +1,11 @@
-export const COLOR_PRIMARY = 'tomato';
+const BG_COLOR = '#F8F5F1';
+const SELECT_COLOR = '#E1FFF8';
+const MAIN_COLOR = '#5AA897';
+const TEXT_COLOR = '#45526C';
+
+export {
+  BG_COLOR,
+  SELECT_COLOR,
+  MAIN_COLOR,
+  TEXT_COLOR,
+};
