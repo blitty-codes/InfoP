7893395c9d5142786bb8b8a260ee7fde70a5907d
blitty-codes <blittynet@gmail.com>
2021-05-07 22:17:09 +0200

    changed the parameter to a string

--- Hooks/useFilm.ts
+++ Hooks/useFilm.ts
@@ -1,13 +1,9 @@
 import { useState, useEffect } from "react";
 import { rapidApiKey } from '../env';
-import { IFilmData } from '../Interface/IFilmData';
+import IFilmData from '../Interface/IFilmData';
 var axios = require("axios").default;
 
-interface IFilm {
-  name: string,
-}
-
-function useFilm(nameFilm: IFilm) {
+function useFilm(nameFilm: string) {
   const [film, setFilm] = useState<IFilmData[]>();
   let films: IFilmData[] = new Array(0);
 
@@ -15,33 +11,33 @@ function useFilm(nameFilm: IFilm) {
     var options = {
       method: "GET",
       url: "https://imdb8.p.rapidapi.com/auto-complete",
-      params: { q: nameFilm.name },
+      params: { q: nameFilm },
       headers: {
         "x-rapidapi-key": rapidApiKey,
         "x-rapidapi-host": "imdb8.p.rapidapi.com",
       },
     };
 
-    if (nameFilm.name != '') {
+    if (nameFilm != '') {
       axios.request(options).then(function (response: any) {
         response.data.d.forEach((film: any) => {
           films.push(
             {
               image: {
-                height: film.i.height,
-                width: film.i.width,
-                imageURL: film.i.imageUrl,
+                height: 0 ?? film.i.height,
+                width: 0 ?? film.i.width,
+                imageURL: null ?? film.i.imageUrl,
               },
               id: film.id,
               title: film.l,
-              rank: film.rank,
-              authors: film.s,
-              year: film.y,
+              rank: null ?? film.rank,
+              authors: '' ?? film.s,
+              year: null ?? film.y,
             }
           )
         });
         setFilm(films);
-        // console.log(film);
+        console.log(response.data.d);
       }).catch(function (error: string) {
         console.error(error);
       });
