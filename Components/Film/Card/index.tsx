d07509a25522f6446528c3819681186d11a9968a
blitty-codes <blittynet@gmail.com>
2021-05-07 22:20:27 +0200

    Divided into components

--- Components/Film/Card/index.tsx
+++ Components/Film/Card/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image } from 'react-native';
 
-import { IFilmData } from '../../../Interface/IFilmData';
+import IFilmData from '../../../Interface/IFilmData';
+import StyledIcon from "../../StyledIcons";
+import filmCardStyle from "../../../style/filmCardStyle";
 
 function CardFilm (props: any): any {
-  const filmResult: IFilmData = props.filmResult;
+  const film: IFilmData = props.filmResult;
+  const [starRated, setStarRated] = useState("staro");
+
   return (
     <View>
-      <Text>{filmResult.title}</Text>
+      <Image style={filmCardStyle.tinyLogo} source={{ uri: film.image.imageURL, }} />
+      <StyledIcon name={starRated} size={36} style={{ width: "fit-content", position: 'absolute', right: 2 }} color="gold" onPress={() => { setStarRated((starRated == "star") ? "staro" : "star") }} />
+      <View style={filmCardStyle.allText}>
+        <Text style={filmCardStyle.title} >{film.title}</Text>
+        <View style={filmCardStyle.description}>
+          <View>
+            <Text style={filmCardStyle.decor} >Rating:</Text>
+            <Text>{film.rank}</Text>
+          </View>
+          <View>
+            <Text style={filmCardStyle.decor}>Authors:</Text>
+            <Text>{film.authors}</Text>
+          </View>
+          <View>
+            <Text style={filmCardStyle.decor}>Year:</Text>
+            <Text>{film.year}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
--- Components/Film/index.tsx
+++ Components/Film/index.tsx
@@ -1,80 +1,28 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, ScrollView, Image } from 'react-native';
+import React, { useContext } from 'react';
+import { View, SafeAreaView, ScrollView } from 'react-native';
 import { FilmContext } from '../Provider';
-import Constants from 'expo-constants';
+import CardFilm from './Card/index';
 
 import useFilm from '../../Hooks/useFilm';
-import { IFilmData } from '../../Interface/IFilmData';
-import StyledIcon from '../StyledIcons';
+import IFilmData from '../../Interface/IFilmData';
+import IFilmUseState from '../../Interface/IFilmUseState';
 
-const style = StyleSheet.create({
-  container: {
-    borderWidth: 5,
-    marginHorizontal: 10,
-    marginVertical: 10,
-    width: "min-content",
-    backgroundColor: "silver"
-  },
-  scrollContainer: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-  scrollViewContainer: {
-    // backgroundColor: "pink",
-    marginHorizontal: 20,
-  },
-  tinyLogo: {
-    margin: "auto",
-    width: 200,
-    height: 250,
-  },
-  allText: {
-    margin: "auto",
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    textAlign: "center",
-  },
-  description: {
-    padding: 10,
-  },
-  decor: {
-    textDecorationLine: "underline",
-    textDecorationStyle: "solid",
-  }
-});
+import scrollViewStyle from '../../style/scollViewStyle';
+import filmCardStyle from '../../style/filmCardStyle';
 
 function Film(): any {
-  const [titleFilm, setTitleFilm] = useContext(FilmContext);
-  const [starRated, setStarRated] = useState("staro");
+  const { titleFilm }: IFilmUseState = useContext(FilmContext);
   const info: (IFilmData[] | undefined) = useFilm(titleFilm);
 
+  console.log(info);
+
   return (
-    <SafeAreaView style={style.scrollContainer}>
-      <ScrollView style={style.scrollViewContainer}>
+    <SafeAreaView style={scrollViewStyle.scrollContainer}>
+      <ScrollView style={scrollViewStyle.scrollViewContainer}>
         {
           info != undefined && info.map((film: IFilmData) => (
-            <View style={style.container} key={film.id}>
-              <Image style={style.tinyLogo} source={{ uri: film.image.imageURL, }} />
-              <StyledIcon name={starRated} size={36} style={{ width: "fit-content", position: 'absolute', right: 2 }} color="gold" onPress={() => { setStarRated((starRated == "star") ? "staro" : "star") }} />
-              <View style={style.allText}>
-                <Text style={style.title} >{film.title}</Text>
-                <View style={style.description}>
-                  <View>
-                    <Text style={style.decor} >Rating:</Text>
-                    <Text>{film.rank}</Text>
-                  </View>
-                  <View>
-                    <Text style={style.decor}>Authors:</Text>
-                    <Text>{film.authors}</Text>
-                  </View>
-                  <View>
-                    <Text style={style.decor}>Year:</Text>
-                    <Text>{film.year}</Text>
-                  </View>
-                </View>
-              </View>
+            <View style={filmCardStyle.container} key={film.id}>
+              <CardFilm filmResult={film} />
             </View>
           ))
         }
